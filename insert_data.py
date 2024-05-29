7896a79e3491ec67d3b8922a60c204306c8ed7e0
Manuel Poirat <mapoirat@gmail.com>
2024-05-29 15:20:52 +0200

    maj compétences

--- insert_data.py
+++ insert_data.py
@@ -4,84 +4,78 @@ import sqlite3
 conn = sqlite3.connect('cv_database.db')
 cursor = conn.cursor()
 
-# Supprimer toutes les données existantes
-cursor.execute("DELETE FROM education")
-cursor.execute("DELETE FROM experience")
-cursor.execute("DELETE FROM skills")
-cursor.execute("DELETE FROM projects")
-
 # Liste des expériences à insérer
 experiences = [
     {
-        "company": "Communauté d'agglomération du Pays Voironnais - Service tourisme ",
-        "job_title": "Chargé de mission base de données",
+        "company": "COMMUNAUTÉ AGGLOMÉRATION PAYS VOIRONNAIS - SERVICE TOURISME",
+        "job_title": "Conception et mise en place d'un observatoire du tourisme pour le territoire",
         "start_date": "2024-03-01",
         "end_date": "2024-09-01",
-        "description": "Conception et mise en place d'un observatoire du tourisme pour le territoire. Récolte, analyse et visualisation des données pour fournir aux décideurs des informations fiables, facilitant ainsi la planification du développement futur du tourisme. Voiron",
-        "skills": ["Excel", "SQL", "Power BI", "Autonomie"]
+        "description": "Récolte, analyse et visualisation des données pour fournir aux décideurs des informations fiables, facilitant ainsi la planification du développement futur du tourisme. Voiron",
+        "skills": ["Excel","SQL", "Power BI", "Autonomie"]
     },
     {
-        "company": "Indépendant",
-        "job_title": "Développeur Wordpress",
+        "company": "DÉVELOPPEUR WORDPRESS INDÉPENDANT",
+        "job_title": "Création et maintenance de sites internet Wordpress",
         "start_date": "2022-01-01",
         "end_date": None,
         "description": "Création et maintenance de sites internet Wordpress.",
-        "skills": ["Autonomie", "Wordpress"]
+        "skills": ["Autonomie","Wordpress"]
     },
     {
-        "company": "Indépendant",
-        "job_title": "Congé sabbatique personnel - Suivi de conjoint",
+        "company": "CONGÉ SABBATIQUE PERSONNEL - SUIVI DE CONJOINT",
+        "job_title": "Expérience de vie aux États-Unis (Denver, Colorado) en famille pendant 2 années",
         "start_date": "2021-07-01",
         "end_date": "2023-08-01",
         "description": "Expérience de vie aux États-Unis (Denver, Colorado) en famille pendant 2 années.",
         "skills": []
     },
     {
-        "company": "Roc France (Areas))",
-        "job_title": "Adjoint de direction",
+        "company": "ADJOINT DE DIRECTION ROC FRANCE (AREAS)",
+        "job_title": "Manager (20 employés) et assistant administratif 3 magasins Franprix et station-service Esso d'autoroute",
         "start_date": "2018-05-01",
         "end_date": "2021-06-01",
-        "description": "Manager (20 employés) et assistant administratif de 2 magasins Franprix et station-service Esso d'autoroute. Drôme",
-        "skills": ["Travail en équipe", "Management", "Excel"]
+        "description": "Manager (20 employés) et assistant administratif 3 magasins Franprix et station-service Esso d'autoroute. Drôme",
+        "skills": ["Travail en équipe","Management","Excel"]
     },
     {
-        "company": "Huttopia",
-        "job_title": "Directeur de site",
+        "company": "DIRECTEUR DE SITE HUTTOPIA",
+        "job_title": "Directeur de villages-vacances",
         "start_date": "2013-01-01",
         "end_date": "2018-02-01",
         "description": "Manager de 20 personnes. Font-Romeu & Divonne-les-Bains",
-        "skills": ["Travail en équipe", "Management"]
+        "skills": ["Travail en équipe","Management"]
     },
     {
-        "company": "Événements et Voyages",
+        "company": "CHARGÉ DE PROJETS ÉVÉNEMENTS ET VOYAGES",
         "job_title": "Agent de voyages multitâches",
         "start_date": "2010-09-01",
         "end_date": "2013-01-01",
         "description": "Agent de voyages multitâches. Lyon",
-        "skills": ["Autonomie","Voyage"]
+        "skills": []
     },
     {
-        "company": "Petit Futé",
-        "job_title": "Commercial",
+        "company": "COMMERCIAL PETIT FUTÉ",
+        "job_title": "Création d'un portefeuille de 40 clients (hôtels et agences de voyages principalement) en Tanzanie et 60 en Afrique du Sud",
         "start_date": "2009-01-01",
         "end_date": "2010-12-01",
         "description": "Création d'un portefeuille de 40 clients (hôtels et agences de voyages principalement) en Tanzanie et 60 en Afrique du Sud. Auto-entrepreneur",
         "skills": ["Autonomie"]
     },
     {
-        "company": "Nacel - American Village",
-        "job_title": "Directeur de site",
+        "company": "DIRECTEUR DE SITE AMERICAN VILLAGE",
+        "job_title": "Manager d'une équipe d'animateurs anglophones et responsable de 50 enfants par quinzaine",
         "start_date": "2007-06-01",
         "end_date": "2010-09-01",
         "description": "Manager d'une équipe d'animateurs anglophones et responsable de 50 enfants par quinzaine. Mâcon",
-        "skills": ["Travail en équipe"]
+        "skills": []
     },
     {
-        "company": "Magic in Motion",
+        "company": "RESPONSABLE JARDIN D'ENFANTS MAGIC IN MOTION",
         "job_title": "Responsable du jardin d'enfants, aide-moniteur, création d'événements",
         "start_date": "2006-12-01",
         "end_date": "2007-03-01",
-        "description": "Responsable du jardin d'enfants, aide-moniteur, création d'événements. Courchevel",
+        "description": "Responsable du jardin d'enfants, aide-moniteur, création d'événements.Courchevel",
         "skills": ["Travail en équipe"]
     }
 ]
@@ -98,6 +92,10 @@ for exp in experiences:
             INSERT INTO experience_skills (experience_id, skill_name)
             VALUES (?, ?)
         """, (exp_id, skill))
+        cursor.execute("""
+            INSERT OR IGNORE INTO skills (skill_name, proficiency)
+            VALUES (?, ?)
+        """, (skill, 1))  # Proficiency par défaut à 1 si la compétence est nouvelle
 
 # Liste des formations à insérer
 formations = [
@@ -108,7 +106,7 @@ formations = [
         "end_date": "2024-07-01",
         "description": "Base de données, Analyse et traitement de données, Data visualisation, Analyse statistique, Python, Jupyter, SQL, Power BI",
         "field_of_study": "Data Analysis",
-        "skills": ["Python", "Jupyter", "SQL", "Power BI", "Travail en équipe", "Autonomie"]
+        "skills": ["Python", "Jupyter", "SQL", "Power BI","Travail en équipe", "Autonomie"]
     },
     {
         "institution": "WPChef",
@@ -117,7 +115,7 @@ formations = [
         "end_date": "2021-12-01",
         "description": "Certification pro. développeur Wordpress.",
         "field_of_study": "Développement Web",
-        "skills": ["Wordpress", "Autonomie"]
+        "skills": ["Wordpress","Autonomie"]
     },
     {
         "institution": "UJF Grenoble",
@@ -133,15 +131,19 @@ formations = [
 # Insérer les formations dans la base de données
 for edu in formations:
     cursor.execute("""
-        INSERT INTO education (institution, degree, start_date, end_date, description, field_of_study)
-        VALUES (?, ?, ?, ?, ?, ?)
-    """, (edu['institution'], edu['degree'], edu['start_date'], edu['end_date'], edu['description'], edu['field_of_study']))
+        INSERT INTO education (institution, degree, start_date, end_date, description)
+        VALUES (?, ?, ?, ?, ?)
+    """, (edu['institution'], edu['degree'], edu['start_date'], edu['end_date'], edu['description']))
     edu_id = cursor.lastrowid
     for skill in edu['skills']:
         cursor.execute("""
             INSERT INTO education_skills (education_id, skill_name)
             VALUES (?, ?)
         """, (edu_id, skill))
+        cursor.execute("""
+            INSERT OR IGNORE INTO skills (skill_name, proficiency)
+            VALUES (?, ?)
+        """, (skill, 1))  # Proficiency par défaut à 1 si la compétence est nouvelle
 
 # Commit des modifications et fermer la connexion
 conn.commit()

bb4dd61a60af783d4ed3704b21f32a4e8a52b308
Manuel Poirat <mapoirat@gmail.com>
2024-05-29 14:32:00 +0200

    maj data

--- insert_data.py
+++ insert_data.py
@@ -13,71 +13,71 @@ cursor.execute("DELETE FROM projects")
 # Liste des expériences à insérer
 experiences = [
     {
-        "company": "COMMUNAUTÉ AGGLOMÉRATION PAYS VOIRONNAIS - SERVICE TOURISME",
-        "job_title": "Conception et mise en place d'un observatoire du tourisme pour le territoire",
+        "company": "Communauté d'agglomération du Pays Voironnais - Service tourisme ",
+        "job_title": "Chargé de mission base de données",
         "start_date": "2024-03-01",
         "end_date": "2024-09-01",
-        "description": "Récolte, analyse et visualisation des données pour fournir aux décideurs des informations fiables, facilitant ainsi la planification du développement futur du tourisme. Voiron",
+        "description": "Conception et mise en place d'un observatoire du tourisme pour le territoire. Récolte, analyse et visualisation des données pour fournir aux décideurs des informations fiables, facilitant ainsi la planification du développement futur du tourisme. Voiron",
         "skills": ["Excel", "SQL", "Power BI", "Autonomie"]
     },
     {
-        "company": "DÉVELOPPEUR WORDPRESS INDÉPENDANT",
-        "job_title": "Création et maintenance de sites internet Wordpress",
+        "company": "Indépendant",
+        "job_title": "Développeur Wordpress",
         "start_date": "2022-01-01",
         "end_date": None,
         "description": "Création et maintenance de sites internet Wordpress.",
         "skills": ["Autonomie", "Wordpress"]
     },
     {
-        "company": "CONGÉ SABBATIQUE PERSONNEL - SUIVI DE CONJOINT",
-        "job_title": "Expérience de vie aux États-Unis (Denver, Colorado) en famille pendant 2 années",
+        "company": "Indépendant",
+        "job_title": "Congé sabbatique personnel - Suivi de conjoint",
         "start_date": "2021-07-01",
         "end_date": "2023-08-01",
         "description": "Expérience de vie aux États-Unis (Denver, Colorado) en famille pendant 2 années.",
         "skills": []
     },
     {
-        "company": "ADJOINT DE DIRECTION ROC FRANCE (AREAS)",
-        "job_title": "Manager (20 employés) et assistant administratif 3 magasins Franprix et station-service Esso d'autoroute",
+        "company": "Roc France (Areas))",
+        "job_title": "Adjoint de direction",
         "start_date": "2018-05-01",
         "end_date": "2021-06-01",
-        "description": "Manager (20 employés) et assistant administratif 3 magasins Franprix et station-service Esso d'autoroute. Drôme",
+        "description": "Manager (20 employés) et assistant administratif de 2 magasins Franprix et station-service Esso d'autoroute. Drôme",
         "skills": ["Travail en équipe", "Management", "Excel"]
     },
     {
-        "company": "DIRECTEUR DE SITE HUTTOPIA",
-        "job_title": "Directeur de villages-vacances",
+        "company": "Huttopia",
+        "job_title": "Directeur de site",
         "start_date": "2013-01-01",
         "end_date": "2018-02-01",
         "description": "Manager de 20 personnes. Font-Romeu & Divonne-les-Bains",
         "skills": ["Travail en équipe", "Management"]
     },
     {
-        "company": "CHARGÉ DE PROJETS ÉVÉNEMENTS ET VOYAGES",
+        "company": "Événements et Voyages",
         "job_title": "Agent de voyages multitâches",
         "start_date": "2010-09-01",
         "end_date": "2013-01-01",
         "description": "Agent de voyages multitâches. Lyon",
-        "skills": []
+        "skills": ["Autonomie","Voyage"]
     },
     {
-        "company": "COMMERCIAL PETIT FUTÉ",
-        "job_title": "Création d'un portefeuille de 40 clients (hôtels et agences de voyages principalement) en Tanzanie et 60 en Afrique du Sud",
+        "company": "Petit Futé",
+        "job_title": "Commercial",
         "start_date": "2009-01-01",
         "end_date": "2010-12-01",
         "description": "Création d'un portefeuille de 40 clients (hôtels et agences de voyages principalement) en Tanzanie et 60 en Afrique du Sud. Auto-entrepreneur",
         "skills": ["Autonomie"]
     },
     {
-        "company": "DIRECTEUR DE SITE AMERICAN VILLAGE",
-        "job_title": "Manager d'une équipe d'animateurs anglophones et responsable de 50 enfants par quinzaine",
+        "company": "Nacel - American Village",
+        "job_title": "Directeur de site",
         "start_date": "2007-06-01",
         "end_date": "2010-09-01",
         "description": "Manager d'une équipe d'animateurs anglophones et responsable de 50 enfants par quinzaine. Mâcon",
-        "skills": []
+        "skills": ["Travail en équipe"]
     },
     {
-        "company": "RESPONSABLE JARDIN D'ENFANTS MAGIC IN MOTION",
+        "company": "Magic in Motion",
         "job_title": "Responsable du jardin d'enfants, aide-moniteur, création d'événements",
         "start_date": "2006-12-01",
         "end_date": "2007-03-01",
